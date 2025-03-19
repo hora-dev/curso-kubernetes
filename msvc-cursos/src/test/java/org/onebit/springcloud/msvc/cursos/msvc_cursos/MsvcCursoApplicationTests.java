70a57214943e071ba1a657ca0815ab19018878b8
horadev <horacio@megawebs.com.ar>
2025-03-19 18:54:15 -0300

    test curso

---
+++ msvc-cursos/src/test/java/org/onebit/springcloud/msvc/cursos/msvc_cursos/MsvcCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.onebit.springcloud.msvc.cursos.msvc_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
